b3018cdd0e75f32ef28e1ea10ed97ecefce22e57
shakib8080 <mdshakibulhasan0808@gmail.com>
2024-06-09 18:51:16 +0600

    first commit

---
+++ array-index.js
@@ -0,0 +1,7 @@
+var bookspage = ["am" ,"jam" ,"kathal","peyara"];
+
+// console.log(bookspage.indexOf("kathal"));
+
+var numbers = [1 ,2 ,2,4,5,6,7];
+ var fundindex = bookspage [2];
+ console.log(fundindex);
---
+++ array.js
@@ -0,0 +1,12 @@
+var friendsAge = [12, 13,15,18];
+
+var myfriendsname = ["shakib","rakib","ashik"];
+
+console.log(friendsAge);
+console.log(myfriendsname.length);
+
+var oddnumbers = [1,2,3,4,5];
+var vowels = ["a","b","c","d","e"];
+
+console.log(oddnumbers);
+console.log(vowels.length);
